6265419cfd63c7f822361d7afffe728da861f828
wongben16 <wongben16@gmail.com>
2023-01-26 16:58:56 -0500

    Program is working, need to clean up how it prints the score.

--- Bowling.js
+++ Bowling.js
@@ -4,81 +4,136 @@
 // var myInterface = readline.createInterface({
 //     input: fs.createReadStream('demofile1.html')
 // });
-
-const originalScore = 'x-x-x-x-x-x-x-x-45-54'
-
-originalScore.toLowerCase();
-
 // let originalScore = readline.question("Enter in your bowling scores.");
-const throws = originalScore.split('-').join(''); //removes the "-" in the scores and rejoins the array toegether with ""
+// const originalScore = 'x-x-x-x-x-x-x-x-x-x-4-5';
+// const originalScore = 'x-x-x-x-x-x-x-x-45-54';
+const originalScore = '5/-43-10-x-x-x-9/-4/-x-x-6-0';
 
 
+const throws = originalScore.split('-').join('').toLowerCase(); //removes the "-" in the scores and rejoins the array toegether with ""
 const frameScoreValue = [];
 var throwsIterator = 0;
-var scoreIterator = 0;
 var totalScore = 0;
 var frameCounter = 0;
 
-/////////////////debug the program. not working as of 01.24.23//////////////////
 
-
-while(throwsIterator < throws.length){
-    // console.log(throws[throwsIterator])
-    if(/\d/.test(throws[throwsIterator])){ //checks if string contains a number 0-9. True if there is a number
-
-        if (/\d/.test(throws[throwsIterator+1])){ //if throw 1 and 2 are numbers then adds them together
-            frameScoreValue[scoreIterator] = parseInt(throws[throwsIterator+1]) + parseInt(throws[throwsIterator+2]);
-            console.log(frameScoreValue[scoreIterator]);
-        }
-
-        else { //if throw 2 is a / then score is 10 + the next throw (which is the third throw)
-            frameScoreValue[scoreIterator] = 10 + parseInt(throws[throwsIterator+2]);
-            throwsIterator++;
-        }
-    }
-
-    else {
-        if (throws[throwsIterator] == "x"){//checks if string where throwsIterator is contains a X
-           
-            if (throws[throwsIterator+1] == "x"){ //checks if next number in the string is a X
-                
-                if (throws[throwsIterator+2] == "x"){ //checks if second throw is a X
-                    frameScoreValue[scoreIterator] = 30;
-                }
-
-                else{
-                    frameScoreValue[scoreIterator] = (20 + parseInt(throws[throwsIterator+2]));
-                }
-            }
-
-            else if (throws[throwsIterator+2] == "/"){ // check if second throw is a /
-               frameScoreValue[scoreIterator] = 20;
-
-            }
-               
-            else { //adds X + next two throw if throws did not contain X or /
-                frameScoreValue[scoreIterator] = 10 + parseInt(throws[throwsIterator+1]) + parseInt(throws[throwsIterator+2]);
-            }    
-
-        }
-
-        else if(throws[throwsIterator+1] == "/"){ //checks if second throw is a /
-            
-            if (throws[throwsIterator+2] == "x"){ // check if second throw is a X
-                frameScoreValue[scoreIterator] = 20;
-             }
-
-        }
-    }
-
-    scoreIterator++;
-    throwsIterator++;
-    
+function parseIfXValue(value) {
+    if (value === 'x') return 10;
+    return parseInt(value)
 }
+function parseValue(firstThrow, secondThrow) {
+    console.log("-------------Inside parseValue Function------------");
+    console.log("firstThrow = ", firstThrow);
+    console.log("secondThrow = ", secondThrow);
+
+    if (secondThrow === '/'){
+        return 10;
+    }
+    else if (firstThrow === 'x'){
+        if (secondThrow === 'x'){
+            return 20;
+        }
+        return 10 + parseInt(secondThrow);
+    }
+    else{
+        return parseInt(firstThrow) + parseInt(secondThrow);
+    }
+}
+
+while(throwsIterator < throws.length && frameCounter < 10){
+    var currentThrow = throws[throwsIterator];
+    var nextThrow = throws[throwsIterator+1];
+    var secondNextThrow = throws[throwsIterator+2];
+
+    console.log("/////////////Inside While Loop//////////////");
+    console.log("currentThrow = " , currentThrow);
+    console.log("nextThrow = " , nextThrow);
+    console.log("nextSecondThrow = ", secondNextThrow);
+    
+    
+    if (currentThrow === "x"){
+        frameScoreValue[frameCounter] = 10
+        frameScoreValue[frameCounter] += parseValue(nextThrow, secondNextThrow); 
+        throwsIterator++;   
+    }
+    // else if (nextThrow === "/"){
+    //     frameScoreValue[frameCounter] = parseValue(currentThrow, secondNextThrow); 
+    //     throwsIterator = throwsIterator + 2; 
+    // }
+    else{
+        if (nextThrow === "/"){
+            frameScoreValue[frameCounter] = 10 + parseIfXValue(secondNextThrow);
+            throwsIterator = throwsIterator + 2;
+        }
+        else {
+            frameScoreValue[frameCounter] = parseValue(currentThrow, nextThrow);
+            throwsIterator = throwsIterator + 2;
+        }
+    }
+    frameCounter++;
+}
+
+
 
 for (i = 0; i < frameScoreValue.length; i++){
     totalScore= totalScore + frameScoreValue[i];
     console.log(frameScoreValue);
-    console.log(totalScore);
+    console.log("Total Score = ", totalScore);
 }
+
+
+
+// while(throwsIterator < throws.length && frameCounter < 10){
+//     // console.log(throws[throwsIterator])
+//     if(/\d/.test(throws[throwsIterator])){ //checks if string contains a number 0-9. True if there is a number
+
+//         if (/\d/.test(throws[throwsIterator+1])){ //if throw 1 and 2 are numbers then adds them together
+//             frameScoreValue[frameCounter] = parseValue(throws[throwsIterator+1]) + parseValue(throws[throwsIterator+2]);
+//             console.log(frameScoreValue[scoreIterator]);
+//         }
+
+//         else { //if throw 2 is a / then score is 10 + the next throw (which is the third throw)
+//             frameScoreValue[scoreIterator] = 10 + parseValue(throws[throwsIterator+2]);
+//             throwsIterator++;
+//         }
+//     }
+
+//     else {
+//         if (throws[throwsIterator] == "x"){//checks if string where throwsIterator is contains a X
+           
+//             if (throws[throwsIterator+1] == "x"){ //checks if next number in the string is a X
+                
+//                 if (throws[throwsIterator+2] == "x"){ //checks if second throw is a X
+//                     frameScoreValue[scoreIterator] = 30;
+//                 }
+
+//                 else{
+//                     frameScoreValue[scoreIterator] = (20 + parseValue(throws[throwsIterator+2]));
+//                 }
+//             }
+
+//             else if (throws[throwsIterator+2] == "/"){ // check if second throw is a /
+//                frameScoreValue[scoreIterator] = 20;
+
+//             }
+               
+//             else { //adds X + next two throw if throws did not contain X or /
+//                 frameScoreValue[scoreIterator] = 10 + parseValue(throws[throwsIterator+1]) + parseValue(throws[throwsIterator+2]);
+//             }    
+
+//         }
+
+//         else if(throws[throwsIterator+1] == "/"){ //checks if second throw is a /
+            
+//             if (throws[throwsIterator+2] == "x"){ // check if second throw is a X
+//                 frameScoreValue[scoreIterator] = 20;
+//              }
+
+//         }
+//     }
+
+//     scoreIterator++;
+//     throwsIterator++;
+    
+// }
 
